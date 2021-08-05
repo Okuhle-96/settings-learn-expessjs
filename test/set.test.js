b9eddc31dc48f8f47dcd9af99334c3ddc1719028
Okuhle_Mhambi <bukhosimhambi@gmail.com>
2021-08-05 16:39:37 +0200

    dsjhb

--- test/set.test.js
+++ test/set.test.js
@@ -1,5 +1,5 @@
 let assert = require("assert");
-let SettingsBill = require("./express-intro/setting-bill");
+let SettingsBill = require("../setting-bill");
 
 
 describe('Setting Bill Factory Function', function(){
